d84ebe59dc069cc50af1e0571d8023a389cd22ca
jacky32 <patrki@atlas.cz>
2022-02-01 17:56:49 +0100

    shortened

--- Caesar-cipher.rb
+++ Caesar-cipher.rb
@@ -1,24 +1,15 @@
 def caesar_cipher(string, shift_factor)
   altered_string = string.split("").map do |symb|
-    if ('a'..'z').to_a.include?(symb)
-      symb_index = ('a'..'z').to_a.index(symb)
-      #overflow compatibility
-      if symb_index + shift_factor > 26
-        overflow_index = symb_index + shift_factor - 26
-        ('a'..'z').to_a[overflow_index]
-      else
-        ('a'..'z').to_a[symb_index+shift_factor]
-      end
+    if ('a'..'z').to_a.include?(symb) #checks whether symbol is UPPER/lower or not a letter
+      symb_index = ('a'..'z').to_a.index(symb) 
+      overflow_index = symb_index + shift_factor
+      overflow_index -= 26 while overflow_index > 25 #overflow compatibility
+      ('a'..'z').to_a[overflow_index] #returns transformed letter
     elsif ('A'..'Z').to_a.include?(symb)
       symb_index = ('A'..'Z').to_a.index(symb)
-      p symb_index
-      #overflow compatibility
-      if symb_index + shift_factor > 26
-        overflow_index = symb_index + shift_factor - 26
-        ('A'..'Z').to_a[overflow_index]
-      else
-        ('A'..'Z').to_a[symb_index+shift_factor]
-      end
+      overflow_index = symb_index + shift_factor
+      overflow_index -= 26 while overflow_index > 25
+      ('A'..'Z').to_a[overflow_index]
     else
       symb
     end

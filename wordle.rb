452e12b7e58e7c77dc6037d19287f8660d954c59
jacky32 <patrki@atlas.cz>
2022-02-12 22:11:15 +0100

    Initial commit

---
+++ wordle.rb
@@ -0,0 +1,30 @@
+class Game
+  @@dictionary = ['about', 'above', 'alert', 'month', 'model', 'lease', 'laugh', 'suite', 'staff', 'young', 'close', 'clean']
+  def initialize
+
+  end
+
+  def start_game
+
+  end
+
+  def new_word
+
+  end
+
+  def print_board 
+
+  end
+
+
+end
+
+class Player
+  def initialize
+
+  end
+
+  def guess
+
+  end
+end
